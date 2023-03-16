7dd57c622c1fcf6d7117429c125a871d9997cba6
ilyas0a <115528846+ilyas0a@users.noreply.github.com>
2023-03-17 02:30:41 +0300

    Add files via upload

---
+++ ilyass.js
@@ -0,0 +1,8 @@
+function pepsii(anything) {
+    document.querySelector('.pepsi').src=anything;
+    
+}
+function ilyastd(color){
+    const continer = document.querySelector('.continer');
+    continer.style.background = color ;
+}
